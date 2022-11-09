532c6d820b9dead36eb47ffe5ff9c53c91aa44d0
johngjackson0 <johngjackson0@gmail.com>
2022-11-09 07:50:21 -0500

    refactor: screens to shared widgets

--- lib/lists/view/createList/create_list_view.dart
+++ lib/lists/view/createList/create_list_view.dart
@@ -4,6 +4,7 @@ import 'package:staty/lists/bloc/lists_bloc.dart';
 import '../../model/form_submission_status.dart';
 import '../../model/list_model.dart';
 import '../widgets/editable_data_point_view.dart';
+import '../widgets/recently_entered_data.dart';
 
 class CreateList extends StatelessWidget {
   const CreateList({super.key});
@@ -32,94 +33,12 @@ class CreateList extends StatelessWidget {
                 return state.formStatus is SubmissionFailed || filter.isEmpty
                     ? const Text('Something went wrong.')
                     : Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        verticalDirection: VerticalDirection.down,
-                        children: <Widget>[
+                        children: [
                           const SizedBox(
                               height: 85,
                               child: Flexible(child: EditableDataPoint())),
-                          const Padding(
-                            padding: EdgeInsets.all(6.0),
-                            child: SizedBox(
-                                height: 25,
-                                child:
-                                    Flexible(child: Text('Recently Entered:'))),
-                          ),
-                          BlocBuilder<ListsBloc, ListsState>(
-                            builder: (context, state) {
-                              return state.formStatus is FormSubmitting
-                                  ? const CircularProgressIndicator()
-                                  : filter[0].data.isEmpty
-                                      ? const Padding(
-                                          padding: EdgeInsets.all(8.0),
-                                          child: Text(
-                                              'There is nothing in the list. Add a new data point to see recently entered data here.',
-                                              style: TextStyle(
-                                                  color: Colors.deepOrange)),
-                                        )
-                                      : Expanded(
-                                          child: Row(
-                                            children: [
-                                              const Align(
-                                                  alignment: Alignment.topLeft,
-                                                  child: Padding(
-                                                    padding: EdgeInsets.only(
-                                                        top: 4,
-                                                        bottom: 4,
-                                                        left: 4,
-                                                        right: 4),
-                                                    child: Flexible(
-                                                        child: Icon(Icons
-                                                            .data_array_sharp)),
-                                                  )),
-                                              Expanded(
-                                                child: Padding(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          top: 4,
-                                                          bottom: 4,
-                                                          left: 16,
-                                                          right: 4),
-                                                  child: Align(
-                                                    alignment:
-                                                        Alignment.topLeft,
-                                                    child: Container(
-                                                      constraints:
-                                                          const BoxConstraints(
-                                                              minHeight: 110,
-                                                              maxHeight: 110),
-                                                      child:
-                                                          SingleChildScrollView(
-                                                        child: Wrap(
-                                                            runSpacing: 12.0,
-                                                            spacing: 12.0,
-                                                            direction:
-                                                                Axis.horizontal,
-                                                            alignment:
-                                                                WrapAlignment
-                                                                    .start,
-                                                            runAlignment:
-                                                                WrapAlignment
-                                                                    .start,
-                                                            children: filter[0]
-                                                                .data
-                                                                .reversed
-                                                                .map((item) =>
-                                                                    DataPointView(
-                                                                        item: item
-                                                                            .value))
-                                                                .toList()),
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        );
-                            },
-                          ),
+                          Flexible(
+                              child: RecentlyEnteredData(list: filter[0].data)),
                         ],
                       );
               },
@@ -127,41 +46,6 @@ class CreateList extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class DataPointView extends StatelessWidget {
-  const DataPointView({
-    Key? key,
-    required this.item,
-  }) : super(key: key);
-
-  final double item;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        context.read<ListsBloc>().add(DeleteDataPointSubmitted(point: item));
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(3.0),
-        child: Wrap(
-          children: [
-            const Icon(Icons.delete_forever_outlined, color: Colors.red),
-            Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Text.rich(
-                TextSpan(
-                  text: item.toString(),
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/lists/view/widgets/recently_entered_data.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/lists_bloc.dart';
+import '../../model/data_point.dart';
+import '../../model/form_submission_status.dart';
+
+class RecentlyEnteredData extends StatelessWidget {
+  const RecentlyEnteredData({
+    Key? key,
+    required this.list,
+  }) : super(key: key);
+
+  final List<DataPoint> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      verticalDirection: VerticalDirection.down,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.all(6.0),
+          child: SizedBox(
+              height: 25, child: Flexible(child: Text('Recently Entered:'))),
+        ),
+        BlocBuilder<ListsBloc, ListsState>(
+      builder: (context, state) {
+        return state.formStatus is FormSubmitting
+            ? const CircularProgressIndicator()
+            : list.isEmpty
+                ? const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                        'There is nothing in the list. Add a new data point to see recently entered data here.',
+                        style: TextStyle(color: Colors.deepOrange)),
+                  )
+                : Expanded(
+                    child: Row(
+                      children: [
+                        const Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  top: 4, bottom: 4, left: 4, right: 4),
+                              child:
+                                  Flexible(child: Icon(Icons.data_array_sharp)),
+                            )),
+                            _DataPointList(list: list),
+                          ],
+                        ),
+                      );
+          },
+        )
+      ],
+    );
+  }
+}
+
+class _DataPointList extends StatelessWidget {
+  const _DataPointList({
+    Key? key,
+    required this.list,
+  }) : super(key: key);
+
+  final List<DataPoint> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 4, left: 16, right: 4),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            // has to maintain cutoff size for datapoint
+            constraints: const BoxConstraints(minHeight: 110, maxHeight: 110),
+            child: SingleChildScrollView(
+              child: Wrap(
+                  runSpacing: 12.0,
+                  spacing: 12.0,
+                  direction: Axis.horizontal,
+                  alignment: WrapAlignment.start,
+                  runAlignment: WrapAlignment.start,
+                  children: list.reversed
+                      .map((item) => _DataPoint(item: item.value))
+                      .toList()),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DataPoint extends StatelessWidget {
+  const _DataPoint({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final double item;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<ListsBloc>().add(DeleteDataPointSubmitted(point: item));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Wrap(
+          children: [
+            const Icon(Icons.delete_forever_outlined, color: Colors.red),
+            Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Text.rich(
+                TextSpan(
+                  text: item.toString(),
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
