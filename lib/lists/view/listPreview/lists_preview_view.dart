8ec989c5773d57bfb9a9e0edca7c04729b07d335
johngjackson0 <johngjackson0@gmail.com>
2022-11-10 07:02:47 -0500

    feat: radio expanded lists

--- lib/lists/view/listPreview/lists_preview_view.dart
+++ lib/lists/view/listPreview/lists_preview_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../bloc/bloc_exports.dart';
+import '../../model/model_exports.dart';
 import '../createList/create_list_view.dart';
 
 class ListsPreview extends StatelessWidget {
@@ -24,29 +25,7 @@ class ListsPreview extends StatelessWidget {
                       ? const Text(
                           'Inferential statistics (like T-test, Z-test, ect.) can have functions that take a list of data or descriptions of the list of data. We only have the latter currently. Please add a list with + icon.',
                           maxLines: 4)
-                      : ListView.builder(
-                          itemCount: state.listStore.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return GestureDetector(
-                              onTap: () {
-                                context.read<ListsBloc>().add(
-                                    SelectedTaskIdEvent(
-                                        id: state.listStore[index].uid));
-                                Navigator.of(context).pushNamed(CreateList.id);
-                              },
-                              child: Column(
-                                children: [
-                                  ListTile(
-                                      leading: const Icon(Icons.list),
-                                      title: Text(
-                                        state.listStore[index].name,
-                                        style: const TextStyle(
-                                            color: Colors.black, fontSize: 20),
-                                      ))
-                                ],
-                              ),
-                            );
-                          });
+                      : _ListContent(listStore: state.listStore);
                 },
               ),
             ),
@@ -60,6 +39,80 @@ class ListsPreview extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _ListContent extends StatelessWidget {
+  final List<ListModel> listStore;
+  const _ListContent({
+    required this.listStore,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: ExpansionPanelList.radio(
+            children: listStore
+                .map((listStore) => ExpansionPanelRadio(
+                    value: listStore.uid,
+                    headerBuilder: (context, isOpen) =>
+                        _ListHeaderTile(listStore: listStore),
+                    body: _ListBodyTile(listStore: listStore)))
+                .toList()
+                .toList()),
+      ),
+    );
+  }
+}
+
+class _ListBodyTile extends StatelessWidget {
+  final ListModel listStore;
+  const _ListBodyTile({
+    required this.listStore,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: SelectableText.rich(TextSpan(children: [
+        const TextSpan(
+          text: 'Title\n',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        TextSpan(text: listStore.name),
+      ])),
+    );
+  }
+}
+
+class _ListHeaderTile extends StatelessWidget {
+  final ListModel listStore;
+  const _ListHeaderTile({
+    required this.listStore,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.read<ListsBloc>().add(SelectedTaskIdEvent(id: listStore.uid));
+        Navigator.of(context).pushNamed(CreateList.id);
+      },
+      child: Column(
+        children: [
+          ListTile(
+              leading: const Icon(Icons.list),
+              title: Text(
+                listStore.name,
+                style: const TextStyle(color: Colors.black, fontSize: 20),
+              ))
+        ],
+      ),
     );
   }
 }
